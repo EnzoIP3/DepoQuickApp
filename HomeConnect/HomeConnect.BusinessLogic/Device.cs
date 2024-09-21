6db909ea944b96c34dcb6fa865de0379052b456c
Manuel <manurz2121@gmail.com>
2024-09-21 20:14:21 -0300

    green: Constructor_WhenArgumentsAreValid_CreatesInstance passed

--- HomeConnect/HomeConnect.BusinessLogic/Device.cs
+++ HomeConnect/HomeConnect.BusinessLogic/Device.cs
@@ -2,4 +2,26 @@ namespace BusinessLogic;
 
 public class Device
 {
+    public string Name { get; init; }
+
+    public int ModelNumber { get; init; }
+
+    public string Description { get; init; }
+
+    public string MainPhoto { get; init; }
+
+    public string[] SecondaryPhotos { get; init; }
+
+    public string Type { get; init; }
+
+    public Device(string name, int modelNumber, string description, string mainPhoto, string[] secondaryPhotos,
+        string type)
+    {
+        Name = name;
+        ModelNumber = modelNumber;
+        Description = description;
+        MainPhoto = mainPhoto;
+        SecondaryPhotos = secondaryPhotos;
+        Type = type;
+    }
 }

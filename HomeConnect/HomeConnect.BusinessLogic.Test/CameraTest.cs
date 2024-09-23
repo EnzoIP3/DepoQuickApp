87370adbed8af87aeb9cbd817fdede81112c3534
Manuel <manurz2121@gmail.com>
2024-09-23 17:31:47 -0300

    red: Add Constructor_WhenNotExteriorAndNotInterior_ThrowsArgumentException

--- HomeConnect/HomeConnect.BusinessLogic.Test/CameraTest.cs
+++ HomeConnect/HomeConnect.BusinessLogic.Test/CameraTest.cs
@@ -28,4 +28,27 @@ public class CameraTest
         // Assert
         act.Should().NotThrow();
     }
+
+    [TestMethod]
+    public void Constructor_WhenNotExteriorAndNotInterior_ThrowsArgumentException()
+    {
+        // Arrange
+        const string name = "Name";
+        const int modelNumber = 123;
+        const string description = "Description";
+        const string mainPhoto = "https://www.example.com/photo1.jpg";
+        var secondaryPhotos =
+            new List<string> { "https://www.example.com/photo2.jpg", "https://www.example.com/photo3.jpg" };
+        const bool motionDetection = true;
+        const bool personDetection = false;
+        const bool isExterior = false;
+        const bool isInterior = false;
+
+        // Act
+        var act = () => new Camera(name, modelNumber, description, mainPhoto, secondaryPhotos, motionDetection,
+            personDetection, isExterior, isInterior);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
 }

906c1265885e1bcc94c076347918f9b3d2b8750d
Manuel <manurz2121@gmail.com>
2024-09-23 17:26:34 -0300

    red: Add Constructor_WhenArgumentsAreValid_CreatesInstance

---
+++ HomeConnect/HomeConnect.BusinessLogic.Test/CameraTest.cs
@@ -0,0 +1,28 @@
+namespace HomeConnect.BusinessLogic.Test;
+
+[TestClass]
+public class CameraTest
+{
+    [TestMethod]
+    public void Constructor_WhenArgumentsAreValid_CreatesInstance()
+    {
+        // Arrange
+        const string name = "Name";
+        const int modelNumber = 123;
+        const string description = "Description";
+        const string mainPhoto = "https://www.example.com/photo1.jpg";
+        var secondaryPhotos =
+            new List<string> { "https://www.example.com/photo2.jpg", "https://www.example.com/photo3.jpg" };
+        const bool motionDetection = true;
+        const bool personDetection = false;
+        const bool isExterior = true;
+        const bool isInterior = false;
+
+        // Act
+        var act = () => new Camera(name, modelNumber, description, mainPhoto, secondaryPhotos, motionDetection,
+            personDetection, isExterior, isInterior);
+
+        // Assert
+        act.Should().NotThrow();
+    }
+}

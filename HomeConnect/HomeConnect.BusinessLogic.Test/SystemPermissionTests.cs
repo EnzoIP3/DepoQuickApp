acc36b9b6720dfa6bb8e8ea1e98e80b775fb41de
Manuel <manurz2121@gmail.com>
2024-09-24 17:30:31 -0300

    red: Add Constructor_WhenArgumentsAreValid_CreatesInstance

---
+++ HomeConnect/HomeConnect.BusinessLogic.Test/SystemPermissionTests.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+
+namespace HomeConnect.BusinessLogic.Test;
+
+[TestClass]
+public class SystemPermissionTests
+{
+    #region Create
+
+    #region Success
+
+    [TestMethod]
+    public void Constructor_WhenArgumentsAreValid_CreatesInstance()
+    {
+        // Arrange
+        const string value = "create_admin";
+
+        // Act
+        var act = () => new SystemPermission("create_admin");
+
+        // Assert
+        act.Should().NotThrow();
+    }
+
+    #endregion
+
+    #endregion
+}

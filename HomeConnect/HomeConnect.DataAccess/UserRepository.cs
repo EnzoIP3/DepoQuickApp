a65138e9980ea69148951b89018402ceffe7f8ad
Manuel <manurz2121@gmail.com>
2024-09-24 20:14:23 -0300

    green: Exists_WhenUserExists_ReturnsTrue passed

--- HomeConnect/HomeConnect.DataAccess/UserRepository.cs
+++ HomeConnect/HomeConnect.DataAccess/UserRepository.cs
@@ -1,6 +1,34 @@
+using BusinessLogic;
+using Microsoft.EntityFrameworkCore;
+
 namespace HomeConnect.DataAccess;
 
-public class Repository
+public class UserRepository : IUserRepository
 {
-    
+    private readonly DbContext _context;
+
+    public UserRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exists(string email)
+    {
+        return _context.Set<User>().Any(u => u.Email == email);
+    }
+
+    public void Delete(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<User> GetUsers(int currentPage, int pageSize, string? fullNameFilter = null, string? roleFilter = null)
+    {
+        throw new NotImplementedException();
+    }
 }

edf701a7dfdb54ddbd03450a5bfd6774ee66962d
Manuel <manurz2121@gmail.com>
2024-09-24 17:43:03 -0300

    green: Constructor_WhenArgumentsAreValid_CreatesInstance passed

--- HomeConnect/HomeConnect.BusinessLogic.Test/RoleTest.cs
+++ HomeConnect/HomeConnect.BusinessLogic.Test/RoleTest.cs
@@ -1,4 +1,5 @@
 using BusinessLogic;
+using FluentAssertions;
 
 namespace HomeConnect.BusinessLogic.Test;
 
---
+++ HomeConnect/HomeConnect.BusinessLogic/RoleClass.cs
@@ -0,0 +1,13 @@
+namespace BusinessLogic;
+
+public class RoleClass
+{
+    private string Name { get; init; }
+    private List<SystemPermission> Permissions { get; init; }
+
+    public RoleClass(string name, List<SystemPermission> permissions)
+    {
+        Name = name;
+        Permissions = permissions;
+    }
+}

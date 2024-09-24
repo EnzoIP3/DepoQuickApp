2598e47b07e6039b2d97486be254ba7a3e64a383
Manuel <manurz2121@gmail.com>
2024-09-24 20:14:13 -0300

    red: Add Exists_WhenUserExists_ReturnsTrue

--- HomeConnect/HomeConnect.DataAccess.Test/DbContextBuilder.cs
+++ HomeConnect/HomeConnect.DataAccess.Test/DbContextBuilder.cs
@@ -1,6 +1,22 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
 namespace HomeConnect.DataAccess.Test;
 
-public class DbContextBuilder
+internal sealed class DbContextBuilder
 {
-    
+    private static readonly SqliteConnection Connection = new SqliteConnection("Data Source=:memory:");
+
+    public static Context BuildTestDbContext()
+    {
+        var options = new DbContextOptionsBuilder<Context>()
+            .UseSqlite(Connection)
+            .Options;
+
+        Connection.Open();
+
+        var context = new Context(options);
+
+        return context;
+    }
 }
---
+++ HomeConnect/HomeConnect.DataAccess/Context.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLogic;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeConnect.DataAccess;
+
+public class Context(DbContextOptions<Context> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<SystemPermission> Permissions { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Role>().HasData(
+            new Role("Admin", []),
+            new Role("Business Owner", []),
+            new Role("Home Owner", []));
+
+        base.OnModelCreating(modelBuilder);
+    }
+}

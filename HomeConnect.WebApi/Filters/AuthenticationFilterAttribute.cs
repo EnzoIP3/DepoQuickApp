b1ea50573fc1faec062256452fdd9a37257810e7
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-09-27 18:26:46 -0300

    green: test OnAuthorization_WhenEmptyHeaders_ShouldRerturnUnauthenticatedResponse passed

--- HomeConnect.WebApi/Filters/AuthenticationFilterAttribute.cs
+++ HomeConnect.WebApi/Filters/AuthenticationFilterAttribute.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace HomeConnect.WebApi.Filters;
@@ -5,8 +7,23 @@ namespace HomeConnect.WebApi.Filters;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public sealed class AuthenticationFilterAttribute : Attribute, IAuthorizationFilter
 {
+    private const string AuthorizationHeader = "Authorization";
+
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        throw new NotImplementedException();
+        var authorizationHeader = context.HttpContext.Request.Headers[AuthorizationHeader];
+
+        if (string.IsNullOrEmpty(authorizationHeader))
+        {
+            context.Result = new ObjectResult(new
+            {
+                InnerCode = "Unauthenticated",
+                Message = "You are not authenticated"
+            })
+            {
+                StatusCode = (int)HttpStatusCode.Unauthorized
+            };
+            return;
+        }
     }
 }

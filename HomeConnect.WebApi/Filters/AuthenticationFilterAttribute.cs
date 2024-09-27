2e5e981e196bbd218e3fc578ec8ab98a1a17b0b3
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-09-27 18:51:01 -0300

    green: test OnAuthorization_WhenEmptyHeaders_ShouldRerturnUnauthenticatedResponse added and passed

--- HomeConnect.WebApi/Filters/AuthenticationFilterAttribute.cs
+++ HomeConnect.WebApi/Filters/AuthenticationFilterAttribute.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
 
 namespace HomeConnect.WebApi.Filters;
 
@@ -25,5 +26,32 @@ public sealed class AuthenticationFilterAttribute : Attribute, IAuthorizationFil
             };
             return;
         }
+
+        var isAuthorizationFormatNotValid = !IsAuthorizationFormatValid(authorizationHeader);
+        if (isAuthorizationFormatNotValid)
+        {
+            context.Result = new ObjectResult(
+                new
+                {
+                    InnerCode = "InvalidAuthorization",
+                    Message = "The provided authorization header format is invalid"
+                })
+            {
+                StatusCode = (int)HttpStatusCode.Unauthorized
+            };
+            return;
+        }
+    }
+
+    private bool IsAuthorizationFormatValid(StringValues authorizationHeader)
+    {
+        if (!authorizationHeader.ToString().StartsWith("Bearer "))
+        {
+            return false;
+        }
+
+        var token = authorizationHeader.ToString().Substring("Bearer ".Length);
+
+        return Guid.TryParse(token, out _);
     }
 }

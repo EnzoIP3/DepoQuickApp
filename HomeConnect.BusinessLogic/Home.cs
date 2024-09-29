2154cde5b4f42dd729049645358e60429504adbc
Manuel <manurz2121@gmail.com>
2024-09-29 08:07:26 -0300

    green: Constructor_WhenArgumentsAreValid_CreatesInstance passed

--- HomeConnect.BusinessLogic/Home.cs
+++ HomeConnect.BusinessLogic/Home.cs
@@ -2,8 +2,19 @@ namespace BusinessLogic;
 
 public class Home
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public User Owner { get; set; }
+    public string Address { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int MaxMembers { get; set; }
+
     public Home(User owner, string address, double latitude, double longitude, int maxMembers)
     {
-        throw new NotImplementedException();
+        Owner = owner;
+        Address = address;
+        Latitude = latitude;
+        Longitude = longitude;
+        MaxMembers = maxMembers;
     }
 }

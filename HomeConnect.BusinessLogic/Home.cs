51192e04a2e04dc2da81f85f99a024fbd17f1a81
Manuel <manurz2121@gmail.com>
2024-09-29 08:19:48 -0300

    green: Constructor_WhenAddressIsNotRoadAndNumber_ThrowsArgumentException passed

--- HomeConnect.BusinessLogic/Home.cs
+++ HomeConnect.BusinessLogic/Home.cs
@@ -4,7 +4,34 @@ public class Home
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public User Owner { get; set; }
-    public string Address { get; set; }
+
+    private string _address = string.Empty;
+
+    public string Address
+    {
+        get => _address;
+        set
+        {
+            var parts = value.Split(' ');
+            if (parts.Length < 2)
+            {
+                throw new ArgumentException("Address must be road and number");
+            }
+
+            if (!parts.Last().All(char.IsDigit))
+            {
+                throw new ArgumentException("Address must be road and number");
+            }
+
+            if (!parts.Any(part => part.All(char.IsLetter)))
+            {
+                throw new ArgumentException("Address must be road and number");
+            }
+
+            _address = value;
+        }
+    }
+
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public int MaxMembers { get; set; }

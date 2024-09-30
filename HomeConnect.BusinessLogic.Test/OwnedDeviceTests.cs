1f3a01c656c95e2b97d0a721c7c564f5c807ee60
Manuel <manurz2121@gmail.com>
2024-09-30 16:35:35 -0300

    refactor: Change HardwareId property to type Guid

--- HomeConnect.BusinessLogic.Test/OwnedDeviceTests.cs
+++ HomeConnect.BusinessLogic.Test/OwnedDeviceTests.cs
@@ -33,6 +33,6 @@ public class OwnedDeviceTests
         var ownedDevice = new OwnedDevice(home, device);
 
         // Assert
-        ownedDevice.HardwareId.Should().NotBeNull();
+        ownedDevice.HardwareId.Should().NotBeEmpty();
     }
 }
--- HomeConnect.BusinessLogic/OwnedDevice.cs
+++ HomeConnect.BusinessLogic/OwnedDevice.cs
@@ -4,12 +4,11 @@ public class OwnedDevice
 {
     public Home Home { get; init; }
     public Device Device { get; init; }
-    public object? HardwareId { get; init; }
+    public Guid HardwareId { get; } = Guid.NewGuid();
 
     public OwnedDevice(Home home, Device device)
     {
         Home = home;
         Device = device;
-        HardwareId = new object();
     }
 }

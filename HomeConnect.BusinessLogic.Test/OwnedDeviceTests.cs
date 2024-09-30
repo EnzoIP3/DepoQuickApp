d84d1ed2a7f53f913ad11a6bba52a80e7b8cf002
Manuel <manurz2121@gmail.com>
2024-09-30 16:20:48 -0300

    red: Add OwnedDevice test Constructor_WhenArgumentsAreValid_CreatesInstance

---
+++ HomeConnect.BusinessLogic.Test/OwnedDeviceTests.cs
@@ -0,0 +1,23 @@
+using BusinessLogic;
+using FluentAssertions;
+
+namespace HomeConnect.BusinessLogic.Test;
+
+[TestClass]
+public class OwnedDeviceTests
+{
+    [TestMethod]
+    public void Constructor_WhenArgumentsAreValid_CreatesInstance()
+    {
+        // Arrange
+        var home = new Home(new User(), "Main St 123", 12.5, 12.5, 5);
+        var device = new Device("Sensor", 12345, "A sensor", "https://sensor.com/image.png", [],
+            "Sensor");
+
+        // Act
+        var act = () => new OwnedDevice(home, device);
+
+        // Assert
+        act.Should().NotThrow();
+    }
+}
---
+++ HomeConnect.BusinessLogic/OwnedDevice.cs
@@ -0,0 +1,9 @@
+namespace BusinessLogic;
+
+public class OwnedDevice
+{
+    public OwnedDevice(Home home, Device device)
+    {
+        throw new NotImplementedException();
+    }
+}

c126641fb055764240d01cc45b485f57e6d4ea1e
Manuel <manurz2121@gmail.com>
2024-09-30 16:39:13 -0300

    green: Constructor_WhenArgumentsAreValid_SetsConnected passed

--- HomeConnect.BusinessLogic/OwnedDevice.cs
+++ HomeConnect.BusinessLogic/OwnedDevice.cs
@@ -5,7 +5,7 @@ public class OwnedDevice
     public Home Home { get; init; }
     public Device Device { get; init; }
     public Guid HardwareId { get; } = Guid.NewGuid();
-    public bool Connected { get; }
+    public bool Connected { get; } = true;
 
     public OwnedDevice(Home home, Device device)
     {

baf19543bfdbf636bdd175cfc155a73eb7be635f
Martin Salaberry <martinnactive5@gmail.com>
2024-10-01 11:58:44 -0300

    red: add test CreateNotification_WhenCalled_AddsNotificationToRepository

---
+++ HomeConnect.BusinessLogic/NotificationService.cs
@@ -0,0 +1,9 @@
+namespace BusinessLogic;
+
+public class NotificationService
+{
+    public void CreateNotification(OwnedDevice ownedDevice, string @event, User user)
+    {
+        throw new NotImplementedException();
+    }
+}

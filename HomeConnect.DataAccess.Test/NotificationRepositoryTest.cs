c9852ccbe263dbcddb99bd257b98f6aed4185aa7
Martin Salaberry <martinnactive5@gmail.com>
2024-10-01 11:30:06 -0300

    refactor: test Add_WhenNotificationDoesNotExist_ShouldAddNotification now includes required parameters for Notification

--- HomeConnect.DataAccess.Test/NotificationRepositoryTest.cs
+++ HomeConnect.DataAccess.Test/NotificationRepositoryTest.cs
@@ -27,8 +27,13 @@ public class NotificationRepositoryTest
     public void Add_WhenNotificationDoesNotExist_ShouldAddNotification()
     {
         // Arrange
-        var notification = new Notification("Notification", "Notification description", "https://example.com/image.png",
-            "Notification message");
+        var user = new User("name", "surname", "email@email.com", "Password#100", new Role());
+        var device = new Device("Device", 12345, "Device description", "https://example.com/image.png",
+            [], "Sensor");
+        var home = new Home(user, "Adress 3420", 100, 100, 5);
+        var ownedDevice = new OwnedDevice(home, device);
+        var notification = new Notification(Guid.NewGuid(), DateTime.Now, false,
+            "Notification message", ownedDevice, user);
 
         // Act
         _notificationRepository.Add(notification);

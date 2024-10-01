8207410089ae85b96a8da6dceb9ce2cb69df0d06
Martin Salaberry <martinnactive5@gmail.com>
2024-10-01 11:59:33 -0300

    green: CreateNotification_WhenCalled_AddsNotificationToRepository passed

--- HomeConnect.BusinessLogic/NotificationService.cs
+++ HomeConnect.BusinessLogic/NotificationService.cs
@@ -2,8 +2,16 @@ namespace BusinessLogic;
 
 public class NotificationService
 {
+    private INotificationRepository NotificationRepository { get; init; }
+
+    public NotificationService(INotificationRepository notificationRepository)
+    {
+        NotificationRepository = notificationRepository;
+    }
+
     public void CreateNotification(OwnedDevice ownedDevice, string @event, User user)
     {
-        throw new NotImplementedException();
+        var notification = new Notification(Guid.NewGuid(), DateTime.Now, false, @event, ownedDevice, user);
+        NotificationRepository.Add(notification);
     }
 }

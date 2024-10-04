a0da097ba9d2afc71281f31603a27fd3e906e264
Martin Salaberry <martinnactive5@gmail.com>
2024-10-04 14:38:09 -0300

    green: GetDevices_WhenCalled_ReturnsDeviceList passed

--- HomeConnect.BusinessLogic/Devices/Services/DeviceService.cs
+++ HomeConnect.BusinessLogic/Devices/Services/DeviceService.cs
@@ -14,6 +14,15 @@ public class DeviceService : IDeviceService
 
     public PagedData<Device> GetDevices(int? currentPage = null, int? pageSize = null, string deviceNameFilter = null, int? modelNumberFilter = null, string businessNameFilter = null, string deviceTypeFilter = null)
     {
-        throw new NotImplementedException();
+        currentPage ??= 1;
+        pageSize ??= 10;
+        var devices = DeviceRepository.GetDevices((int)currentPage, (int)pageSize, deviceNameFilter, modelNumberFilter, businessNameFilter, deviceTypeFilter);
+        return new PagedData<Device>
+        {
+            Data = devices.Data,
+            Page = devices.Page,
+            PageSize = devices.PageSize,
+            TotalPages = devices.TotalPages
+        };
     }
 }

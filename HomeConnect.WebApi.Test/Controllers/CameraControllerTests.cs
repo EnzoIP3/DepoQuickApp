1cb3865686b1c0207b8d0b02c492581e95dd62df
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-10-04 14:01:49 -0300

    red: add test MovementDetected_WithHardwareId_ReturnsNotifyResponse

--- HomeConnect.WebApi.Test/Controllers/CameraControllerTests.cs
+++ HomeConnect.WebApi.Test/Controllers/CameraControllerTests.cs
@@ -1,6 +1,8 @@
 using BusinessLogic.Devices.Services;
+using BusinessLogic.Notifications.Services;
 using FluentAssertions;
 using HomeConnect.WebApi.Controllers.Camera;
+using HomeConnect.WebApi.Controllers.Sensor;
 using Moq;
 
 namespace HomeConnect.WebApi.Test.Controllers;
@@ -9,12 +11,35 @@ namespace HomeConnect.WebApi.Test.Controllers;
 public class CameraControllerTests
 {
     private Mock<IDeviceService> _deviceServiceMock = null!;
+    private Mock<INotificationService> _notificationServiceMock = null!;
     private CameraController _cameraController = null!;
 
     [TestInitialize]
     public void TestInitialize()
     {
         _deviceServiceMock = new Mock<IDeviceService>();
+        _notificationServiceMock = new Mock<INotificationService>();
         _cameraController = new CameraController(_deviceServiceMock.Object);
+    }
+
+    [TestMethod]
+    public void MovementDetected_WithHardwareId_ReturnsNotifyResponse()
+    {
+        // Arrange
+        var hardwareId = "hardwareId";
+        var args = new NotificationArgs
+        {
+            HardwareId = hardwareId,
+            Date = DateTime.Now,
+            Event = "movement-detected"
+        };
+        _notificationServiceMock.Setup(x => x.Notify(args));
+
+        // Act
+        var result = _cameraController.MovementDetected(hardwareId);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.HardwareId.Should().Be(hardwareId);
     }
 }
--- HomeConnect.WebApi/Controllers/Camera/CameraController.cs
+++ HomeConnect.WebApi/Controllers/Camera/CameraController.cs
@@ -1,9 +1,19 @@
 using BusinessLogic.Devices.Services;
+using BusinessLogic.Notifications.Services;
 using HomeConnect.WebApi.Controllers.Device;
+using HomeConnect.WebApi.Controllers.Device.Models;
+using HomeConnect.WebApi.Controllers.Sensor;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeConnect.WebApi.Controllers.Camera;
 
 [ApiController]
 [Route("cameras")]
-public class CameraController(IDeviceService deviceService) : BaseDeviceController(deviceService);
+public class CameraController(IDeviceService deviceService) : BaseDeviceController(deviceService)
+{
+    [HttpPost("{hardwareId}/movement-detected")]
+    public NotifyResponse MovementDetected([FromRoute] string hardwareId)
+    {
+        throw new NotImplementedException();
+    }
+}

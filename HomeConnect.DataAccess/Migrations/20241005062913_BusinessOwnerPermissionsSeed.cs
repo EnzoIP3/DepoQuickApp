4f405d2d1d1957798092af9a6e330a8b982631c8
Manuel <manurz2121@gmail.com>
2024-10-05 03:29:35 -0300

    Add migrations with business owner permissions seed data

---
+++ HomeConnect.DataAccess/Migrations/20241005062913_BusinessOwnerPermissionsSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HomeConnect.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class BusinessOwnerPermissionsSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Permissions",
+                columns: new[] { "Value", "RoleName" },
+                values: new object[,]
+                {
+                    { "create-business", "BusinessOwner" },
+                    { "create-camera", "BusinessOwner" },
+                    { "create-sensor", "BusinessOwner" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Value",
+                keyValue: "create-business");
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Value",
+                keyValue: "create-camera");
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Value",
+                keyValue: "create-sensor");
+        }
+    }
+}

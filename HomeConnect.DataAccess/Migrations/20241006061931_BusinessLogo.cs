79bb6e772da326969423d7eb038260bd2473864c
Manuel <manurz2121@gmail.com>
2024-10-06 03:21:10 -0300

    Add migrations with business logo

---
+++ HomeConnect.DataAccess/Migrations/20241006061931_BusinessLogo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeConnect.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class BusinessLogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Logo",
+                table: "Businesses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: string.Empty);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logo",
+                table: "Businesses");
+        }
+    }
+}

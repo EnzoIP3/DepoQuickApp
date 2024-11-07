2b7c5a24571a42d3313a340709c063defd4be30c
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-11-07 17:41:29 -0300

    refactor: extract methods in ImporterService

--- HomeConnect.BusinessLogic/Devices/Services/ImporterService.cs
+++ HomeConnect.BusinessLogic/Devices/Services/ImporterService.cs
@@ -26,11 +26,11 @@ public class ImporterService : IImporterService
     {
         IDeviceImporter importer = LoadAssembly.GetImplementation(args.ImporterName, Path);
         var deviceArgs = importer.ImportDevices(args.Route);
-        CreateDevices(deviceArgs, args.User);
+        CreateDevicesFromArgs(deviceArgs, args.User);
         return deviceArgs.Select(deviceArg => deviceArg.Name).ToList();
     }
 
-    private void CreateDevices(List<DeviceArgs> deviceArgs, User user)
+    private void CreateDevicesFromArgs(List<DeviceArgs> deviceArgs, User user)
     {
         foreach (var deviceArg in deviceArgs)
         {
@@ -38,36 +38,46 @@ public class ImporterService : IImporterService
             switch (deviceType)
             {
                 case DeviceType.Camera:
-                    var cameraArgs = new CreateCameraArgs
-                    {
-                        Owner = user,
-                        ModelNumber = deviceArg.ModelNumber,
-                        Name = deviceArg.Name,
-                        Description = deviceArg.Description,
-                        MainPhoto = deviceArg.MainPhoto,
-                        SecondaryPhotos = deviceArg.SecondaryPhotos,
-                        MotionDetection = deviceArg.MotionDetection,
-                        PersonDetection = deviceArg.PersonDetection,
-                        Exterior = deviceArg.IsExterior,
-                        Interior = deviceArg.IsInterior
-                    };
-                    BusinessOwnerService.CreateCamera(cameraArgs);
+                    CreateCamera(user, deviceArg);
                     break;
                 default:
-                    var newDeviceArgs = new CreateDeviceArgs
-                    {
-                        Owner = user,
-                        ModelNumber = deviceArg.ModelNumber,
-                        Name = deviceArg.Name,
-                        Description = deviceArg.Description,
-                        MainPhoto = deviceArg.MainPhoto,
-                        SecondaryPhotos = deviceArg.SecondaryPhotos,
-                        Type = deviceArg.Type
-                    };
-                    BusinessOwnerService.CreateDevice(newDeviceArgs);
+                    CreateDevice(user, deviceArg);
                     break;
             }
         }
+    }
+
+    private void CreateCamera(User user, DeviceArgs deviceArg)
+    {
+        var cameraArgs = new CreateCameraArgs
+        {
+            Owner = user,
+            ModelNumber = deviceArg.ModelNumber,
+            Name = deviceArg.Name,
+            Description = deviceArg.Description,
+            MainPhoto = deviceArg.MainPhoto,
+            SecondaryPhotos = deviceArg.SecondaryPhotos,
+            MotionDetection = deviceArg.MotionDetection,
+            PersonDetection = deviceArg.PersonDetection,
+            Exterior = deviceArg.IsExterior,
+            Interior = deviceArg.IsInterior
+        };
+        BusinessOwnerService.CreateCamera(cameraArgs);
+    }
+
+    private void CreateDevice(User user, DeviceArgs deviceArg)
+    {
+        var newDeviceArgs = new CreateDeviceArgs
+        {
+            Owner = user,
+            ModelNumber = deviceArg.ModelNumber,
+            Name = deviceArg.Name,
+            Description = deviceArg.Description,
+            MainPhoto = deviceArg.MainPhoto,
+            SecondaryPhotos = deviceArg.SecondaryPhotos,
+            Type = deviceArg.Type
+        };
+        BusinessOwnerService.CreateDevice(newDeviceArgs);
     }
 
     public List<string> GetImportFiles()

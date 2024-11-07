a7f36291face51a346f70cdbfa9f7980cc5abb0e
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-11-07 16:30:31 -0300

    green: test GetValidators_WhenCalled_ShouldReturnListOfValidatorInfo added and passed

---
+++ HomeConnect.BusinessLogic.Test/BusinessOwners/ValidatorServiceTests.cs
@@ -0,0 +1,37 @@
+using BusinessLogic.BusinessOwners.Helpers;
+using BusinessLogic.BusinessOwners.Services;
+using ModeloValidador.Abstracciones;
+using Moq;
+
+namespace HomeConnect.BusinessLogic.Test.BusinessOwners;
+
+[TestClass]
+public class ValidatorServiceTests
+{
+    private Mock<IAssemblyInterfaceLoader<IModeloValidador>> _mockAssemblyInterfaceLoader = null!;
+    private ValidatorService _validatorService = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _mockAssemblyInterfaceLoader = new Mock<IAssemblyInterfaceLoader<IModeloValidador>>();
+        _validatorService = new ValidatorService(_mockAssemblyInterfaceLoader.Object);
+    }
+
+    [TestMethod]
+    public void GetValidators_WhenCalled_ShouldReturnListOfValidatorInfo()
+    {
+        // Arrange
+        var validatorName = "ValidatorName";
+        _mockAssemblyInterfaceLoader
+            .Setup(x => x.GetImplementationsList(It.IsAny<string>()))
+            .Returns(new List<string> { validatorName });
+
+        // Act
+        var result = _validatorService.GetValidators();
+
+        // Assert
+        Assert.AreEqual(1, result.Count);
+        Assert.AreEqual(validatorName, result[0].Name);
+    }
+}

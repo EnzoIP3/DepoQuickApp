cef24f7b39a2bd58e53cbc6779b0a80b6607b7a2
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-11-07 17:38:32 -0300

    green: test ImportDevices_WhenCalled_ShouldReturnListOfDeviceNames added and passed

--- HomeConnect.BusinessLogic.Test/Devices/Services/ImporterServiceTests.cs
+++ HomeConnect.BusinessLogic.Test/Devices/Services/ImporterServiceTests.cs
@@ -1,5 +1,13 @@
 using BusinessLogic.BusinessOwners.Helpers;
+using BusinessLogic.BusinessOwners.Models;
+using BusinessLogic.BusinessOwners.Repositories;
+using BusinessLogic.BusinessOwners.Services;
+using BusinessLogic.Devices.Entities;
+using BusinessLogic.Devices.Repositories;
 using BusinessLogic.Devices.Services;
+using BusinessLogic.Roles.Entities;
+using BusinessLogic.Users.Entities;
+using BusinessLogic.Users.Repositories;
 using Moq;
 
 namespace HomeConnect.BusinessLogic.Test.Devices.Services;
@@ -8,15 +16,21 @@ namespace HomeConnect.BusinessLogic.Test.Devices.Services;
 public class ImporterServiceTests
 {
     private Mock<IAssemblyInterfaceLoader<IDeviceImporter>> _mockAssemblyInterfaceLoader = null!;
+    private Mock<IDeviceImporter> _mockDeviceImporter = null!;
+    private Mock<IBusinessOwnerService> _mockBusinessOwnerService = null!;
     private ImporterService _importerService = null!;
 
     [TestInitialize]
     public void Initialize()
     {
-        _mockAssemblyInterfaceLoader = new Mock<IAssemblyInterfaceLoader<IDeviceImporter>>();
-        _importerService = new ImporterService(_mockAssemblyInterfaceLoader.Object);
+        _mockAssemblyInterfaceLoader = new Mock<IAssemblyInterfaceLoader<IDeviceImporter>>(MockBehavior.Strict);
+        _mockDeviceImporter = new Mock<IDeviceImporter>(MockBehavior.Strict);
+        _mockBusinessOwnerService = new Mock<IBusinessOwnerService>(MockBehavior.Strict);
+
+        _importerService = new ImporterService(_mockAssemblyInterfaceLoader.Object, _mockBusinessOwnerService.Object);
     }
 
+    #region GetImporters
     [TestMethod]
     public void GetImporters_WhenCalled_ShouldReturnListOfImporters()
     {
@@ -33,4 +47,113 @@ public class ImporterServiceTests
         Assert.AreEqual(1, result.Count);
         Assert.AreEqual(importerName, result[0]);
     }
+    #endregion
+
+    #region ImportDevices
+    [TestMethod]
+public void ImportDevices_WhenCalled_ShouldReturnListOfDeviceNames()
+{
+    // Arrange
+    var importerName = "ImporterName";
+    var route = "Route";
+    var importDevicesArgs = new ImportDevicesArgs
+    {
+        ImporterName = importerName,
+        Route = route,
+        User = new User("John", "Doe", "email@email.com", "Password123!", new Role())
+    };
+
+    var deviceArgs = new List<DeviceArgs>
+    {
+        new DeviceArgs
+        {
+            Name = "DeviceName",
+            ModelNumber = "ModelNumber",
+            Description = "Description",
+            MainPhoto = "MainPhoto",
+            SecondaryPhotos = new List<string> { "SecondaryPhoto" },
+            Type = "Sensor",
+        },
+        new DeviceArgs
+        {
+            Name = "DeviceName2",
+            ModelNumber = "ModelNumber2",
+            Description = "Description2",
+            MainPhoto = "MainPhoto2",
+            SecondaryPhotos = new List<string>(),
+            Type = "Camera",
+            MotionDetection = true,
+            PersonDetection = false,
+            IsExterior = true,
+            IsInterior = false
+        }
+    };
+    var sensorArgs = new CreateDeviceArgs
+    {
+        Owner = importDevicesArgs.User,
+        ModelNumber = deviceArgs[0].ModelNumber,
+        Name = deviceArgs[0].Name,
+        Description = deviceArgs[0].Description,
+        MainPhoto = deviceArgs[0].MainPhoto,
+        SecondaryPhotos = deviceArgs[0].SecondaryPhotos,
+        Type = deviceArgs[0].Type
+    };
+    var cameraArgs = new CreateCameraArgs
+    {
+        Owner = importDevicesArgs.User,
+        ModelNumber = deviceArgs[1].ModelNumber,
+        Name = deviceArgs[1].Name,
+        Description = deviceArgs[1].Description,
+        MainPhoto = deviceArgs[1].MainPhoto,
+        SecondaryPhotos = deviceArgs[1].SecondaryPhotos,
+        MotionDetection = deviceArgs[1].MotionDetection,
+        PersonDetection = deviceArgs[1].PersonDetection,
+        Exterior = deviceArgs[1].IsExterior,
+        Interior = deviceArgs[1].IsInterior
+    };
+    var deviceNames = deviceArgs.Select(deviceArg => deviceArg.Name).ToList();
+    _mockDeviceImporter
+        .Setup(x => x.ImportDevices(route))
+        .Returns(deviceArgs);
+    _mockAssemblyInterfaceLoader
+        .Setup(x => x.GetImplementation(importerName, It.IsAny<string>()))
+        .Returns(_mockDeviceImporter.Object);
+    _mockBusinessOwnerService
+        .Setup(x => x.CreateDevice(It.IsAny<CreateDeviceArgs>())).Returns(It.IsAny<Device>());
+    _mockBusinessOwnerService
+        .Setup(x => x.CreateCamera(It.IsAny<CreateCameraArgs>())).Returns(It.IsAny<Camera>());
+
+    // Act
+    var result = _importerService.ImportDevices(importDevicesArgs);
+
+    // Assert
+    Assert.AreEqual(deviceNames.Count, result.Count);
+    Assert.AreEqual(deviceNames[0], result[0]);
+    Assert.AreEqual(deviceNames[1], result[1]);
+
+    _mockAssemblyInterfaceLoader.Verify(x => x.GetImplementation(importerName, It.IsAny<string>()), Times.Once);
+    _mockDeviceImporter.Verify(x => x.ImportDevices(route), Times.Once);
+    _mockBusinessOwnerService.Verify(x => x.CreateDevice(It.Is<CreateDeviceArgs>(args =>
+        args.SecondaryPhotos != null &&
+        args.Owner == sensorArgs.Owner &&
+        args.ModelNumber == sensorArgs.ModelNumber &&
+        args.Name == sensorArgs.Name &&
+        args.Description == sensorArgs.Description &&
+        args.MainPhoto == sensorArgs.MainPhoto &&
+        args.SecondaryPhotos.SequenceEqual(sensorArgs.SecondaryPhotos) &&
+        args.Type == sensorArgs.Type)), Times.Once);
+    _mockBusinessOwnerService.Verify(x => x.CreateCamera(It.Is<CreateCameraArgs>(args =>
+        args.SecondaryPhotos != null &&
+        args.Owner == cameraArgs.Owner &&
+        args.ModelNumber == cameraArgs.ModelNumber &&
+        args.Name == cameraArgs.Name &&
+        args.Description == cameraArgs.Description &&
+        args.MainPhoto == cameraArgs.MainPhoto &&
+        args.SecondaryPhotos.SequenceEqual(cameraArgs.SecondaryPhotos) &&
+        args.MotionDetection == cameraArgs.MotionDetection &&
+        args.PersonDetection == cameraArgs.PersonDetection &&
+        args.Exterior == cameraArgs.Exterior &&
+        args.Interior == cameraArgs.Interior)), Times.Once);
+}
+    #endregion
 }
--- HomeConnect.BusinessLogic/Devices/Services/ImporterService.cs
+++ HomeConnect.BusinessLogic/Devices/Services/ImporterService.cs
@@ -1,15 +1,20 @@
 using BusinessLogic.BusinessOwners.Helpers;
 using BusinessLogic.BusinessOwners.Models;
+using BusinessLogic.BusinessOwners.Services;
+using BusinessLogic.Devices.Entities;
+using BusinessLogic.Users.Entities;
 
 namespace BusinessLogic.Devices.Services;
 
 public class ImporterService : IImporterService
 {
-    public ImporterService(IAssemblyInterfaceLoader<IDeviceImporter> loadAssembly)
+    public ImporterService(IAssemblyInterfaceLoader<IDeviceImporter> loadAssembly, IBusinessOwnerService businessOwnerService)
     {
         LoadAssembly = loadAssembly;
+        BusinessOwnerService = businessOwnerService;
     }
 
+    private IBusinessOwnerService BusinessOwnerService { get; }
     private IAssemblyInterfaceLoader<IDeviceImporter> LoadAssembly { get; }
     private static readonly string Path = AppDomain.CurrentDomain.BaseDirectory + "Importers";
     public List<string> GetImporters()
@@ -19,7 +24,50 @@ public class ImporterService : IImporterService
 
     public List<string> ImportDevices(ImportDevicesArgs args)
     {
-        throw new NotImplementedException();
+        IDeviceImporter importer = LoadAssembly.GetImplementation(args.ImporterName, Path);
+        var deviceArgs = importer.ImportDevices(args.Route);
+        CreateDevices(deviceArgs, args.User);
+        return deviceArgs.Select(deviceArg => deviceArg.Name).ToList();
+    }
+
+    private void CreateDevices(List<DeviceArgs> deviceArgs, User user)
+    {
+        foreach (var deviceArg in deviceArgs)
+        {
+            var deviceType = Enum.Parse<DeviceType>(deviceArg.Type);
+            switch (deviceType)
+            {
+                case DeviceType.Camera:
+                    var cameraArgs = new CreateCameraArgs
+                    {
+                        Owner = user,
+                        ModelNumber = deviceArg.ModelNumber,
+                        Name = deviceArg.Name,
+                        Description = deviceArg.Description,
+                        MainPhoto = deviceArg.MainPhoto,
+                        SecondaryPhotos = deviceArg.SecondaryPhotos,
+                        MotionDetection = deviceArg.MotionDetection,
+                        PersonDetection = deviceArg.PersonDetection,
+                        Exterior = deviceArg.IsExterior,
+                        Interior = deviceArg.IsInterior
+                    };
+                    BusinessOwnerService.CreateCamera(cameraArgs);
+                    break;
+                default:
+                    var newDeviceArgs = new CreateDeviceArgs
+                    {
+                        Owner = user,
+                        ModelNumber = deviceArg.ModelNumber,
+                        Name = deviceArg.Name,
+                        Description = deviceArg.Description,
+                        MainPhoto = deviceArg.MainPhoto,
+                        SecondaryPhotos = deviceArg.SecondaryPhotos,
+                        Type = deviceArg.Type
+                    };
+                    BusinessOwnerService.CreateDevice(newDeviceArgs);
+                    break;
+            }
+        }
     }
 
     public List<string> GetImportFiles()

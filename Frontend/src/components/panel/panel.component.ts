19392d71a59a24b911d159cd906ec48820789216
Manuel <manurz2121@gmail.com>
2024-11-09 19:06:49 -0300

    Create panel component

---
+++ Frontend/src/components/panel/panel.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { PanelModule } from "primeng/panel";
+
+@Component({
+    selector: "app-panel",
+    standalone: true,
+    imports: [CommonModule, PanelModule],
+    templateUrl: "./panel.component.html"
+})
+export class PanelComponent {
+    @Input() title: string = '';
+    @Input() icon?: string;
+    @Input() toggleable: boolean = true;
+}

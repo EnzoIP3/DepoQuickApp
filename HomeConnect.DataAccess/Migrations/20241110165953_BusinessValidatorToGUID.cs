a8a27608367f30c408b6f5f212c4cf72fdd15528
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-11-10 14:00:50 -0300

    Make migration BusinessValidatorToGUID

---
+++ HomeConnect.DataAccess/Migrations/20241110165953_BusinessValidatorToGUID.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeConnect.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class BusinessValidatorToGUID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Validator",
+                table: "Businesses",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Validator",
+                table: "Businesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}

30057ebea9e0eacd73a7a4da5095ef924802130a
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-11-14 14:58:01 -0300

    refactor: move deviceInfo into models

--- HomeConnect.WebApi/Controllers/Businesses/BusinessController.cs
+++ HomeConnect.WebApi/Controllers/Businesses/BusinessController.cs
@@ -114,14 +114,3 @@ public class BusinessController(IAdminService adminService, IBusinessOwnerServic
         };
     }
 }
-
-public struct DeviceInfo
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string ModelNumber { get; set; }
-    public string Description { get; set; }
-    public string MainPhoto { get; set; }
-    public List<string> SecondaryPhotos { get; set; }
-    public string Type { get; set; }
-}

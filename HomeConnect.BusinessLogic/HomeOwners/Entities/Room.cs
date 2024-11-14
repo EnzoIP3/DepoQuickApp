82cdb09282a65404f1c6143633275e5c88156e60
Martin Salaberry <martinnactive5@gmail.com>
2024-11-14 11:49:43 -0300

    green: Room_WhenNameIsEmpty_ShouldThrowArgumentException passed

--- HomeConnect.BusinessLogic/HomeOwners/Entities/Room.cs
+++ HomeConnect.BusinessLogic/HomeOwners/Entities/Room.cs
@@ -2,6 +2,19 @@ namespace BusinessLogic.HomeOwners.Entities;
 
 public class Room
 {
+    private string _name = string.Empty;
     public Guid Id { get; set; }
-    public string? Name { get; set; }
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Room name cannot be null or empty.");
+            }
+
+            _name = value;
+        }
+    }
 }
